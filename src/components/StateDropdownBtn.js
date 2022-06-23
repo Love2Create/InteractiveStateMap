df6d8f0e73a28b681c16086fc33bdc000d5b705e
Vicky Lu <vicky.lu@OVX1K92C6DR9MAC.attlocal.net>
2022-06-22 21:04:37 -0400

    resolved monochromatic color blind issue. new build

--- src/components/StateDropdownBtn.js
+++ src/components/StateDropdownBtn.js
@@ -3,22 +3,21 @@ import ArrowGraphic from './ArrowGraphic';
 
 const StateDropdownBtn = (props) => {
 
-   const stateSelectHandler = (e) => {
-    // console.log(e.target.value);
-    props.onStateSelectHandler(e.target.value);
-   }
+    const stateSelectHandler = (e) => {
+        // console.log(e.target.value);
+        props.onStateSelectHandler(e.target.value);
+    }
 
-    return(
+    return (
         <form className="StateDropdownBtn-wrapper">
             <label className="StateDropdownBtn-dropdown__label">
-                <span className="arrow">Select a State with Organic Valley Acres</span>
                 <select title="state selector" id="stateName" value={props.selectedState} onChange={stateSelectHandler}>
-                    <option value="" className="StateDropdownBtn-defaultOption__wrapper">Pick a State <ArrowGraphic /> </option>
-                {Object.keys(props.stateMapData).filter(item => props.stateMapData[item].acres != "0").map (item =>
-                    <option key={item} value={item}>
-                        {item}:  {props.stateMapData[item].acres} acres
-                    </option>
-                )}
+                    <option value="" className="StateDropdownBtn-defaultOption__wrapper">Pick a State</option>
+                    {Object.keys(props.stateMapData).filter(item => props.stateMapData[item].acres != "0").map(item =>
+                        <option key={item} value={item}>
+                            {item}:  {props.stateMapData[item].acres} acres
+                        </option>
+                    )}
                 </select>
             </label>
         </form>
--- src/components/StateMap.js
+++ src/components/StateMap.js
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import './StateMap.css';
 import Legend from './Legend';
 
@@ -6,23 +6,26 @@ const StateMap = (props) => {
 
     const [currentAcres, updateCurrentAcres] = useState('');
     const [currentState, updateCurrentState] = useState('');
-    const [tempState, updateTempState] = useState ('');
-    const [currentMouseX, updateMouseX]= useState (0);
-    const [currentMouseY, updateMouseY]= useState (0);
+    const [tempState, updateTempState] = useState('');
+    const [currentMouseX, updateMouseX] = useState(0);
+    const [currentMouseY, updateMouseY] = useState(0);
+    const [currentStateShape, setStateShape] = useState('');
 
 
     const clickHandler = (e) => {
+        const shape = e.target.getAttribute('d');
         const acresData = e.target.getAttribute('acres');
         const stateData = e.target.getAttribute('id');
         updateCurrentAcres(acresData);
         updateCurrentState(stateData);
+        setStateShape(shape);
 
         props.onStateSelectHandler(stateData);
     };
 
     const mouseEnterHandler = (e) => {
 
-        
+
     }
 
     const mouseLeaveHandler = (e) => {
@@ -41,42 +44,54 @@ const StateMap = (props) => {
 
         updateMouseX(e.clientX);
         updateMouseY(e.clientY);
-        
+
     };
 
 
     return (
         <div className="state-map" aria-hidden="true" role="presentation">
-            <div className={tempState==='' ? 'hoverMsg hide' : 'hoverMsg'} style={{left:`${currentMouseX}px`, top:`${currentMouseY}px`} }>{tempState}</div>
+            <div className={tempState === '' ? 'hoverMsg hide' : 'hoverMsg'} style={{ left: `${currentMouseX}px`, top: `${currentMouseY}px` }}>{tempState}</div>
             <svg x="0" y="0" width="575" height="356" viewBox="174 100 959 593">
-                {Object.keys(props.stateMapData).filter(item=>props.stateMapData[item].acres != "0").map(item => <path
-                    className={props.selectedState === item ? 'selected' : ''} 
-                    id={item} 
-                    key={item} 
-                    d={props.stateMapData[item].shape} 
-                    fill="#988c83"
-                    stroke="white"
-                    strokeWidth="2px"
-                    acres={props.stateMapData[item].acres}
-                    onClick={clickHandler}
-                    onMouseEnter={mouseEnterHandler}
-                    onMouseLeave={mouseLeaveHandler}
-                    onMouseMove={mouseMoveHandler}
-                    onMouseUp={mouseLeaveHandler}
-                />)}
-                {Object.keys(props.stateMapData).filter(item=>props.stateMapData[item].acres === "0").map(item => <path
-                    className={props.selectedState === item ? 'selected' : ''} 
-                    id={item} 
-                    key={item} 
-                    d={props.stateMapData[item].shape} 
-                    fill="#c4c0bc"
-                    stroke="white"
-                    strokeWidth="2px"
-                    acres={props.stateMapData[item].acres}
-                    onMouseEnter={mouseEnterHandler}
-                    onMouseLeave={mouseLeaveHandler}
-                    onMouseMove={mouseMoveHandler}
-                />)}
+                <defs>
+                    <clipPath id="selectionMask">
+                        <path d={currentStateShape} />
+                    </clipPath>
+                </defs>
+                <g>
+                    {Object.keys(props.stateMapData).filter(item => props.stateMapData[item].acres != "0").map(item => <path
+                        className={props.selectedState === item ? 'selected' : ''}
+                        id={item}
+                        key={item}
+                        d={props.stateMapData[item].shape}
+                        fill="#988c83"
+                        stroke="white"
+                        strokeWidth="2px"
+                        acres={props.stateMapData[item].acres}
+                        onClick={clickHandler}
+                        onMouseEnter={mouseEnterHandler}
+                        onMouseLeave={mouseLeaveHandler}
+                        onMouseMove={mouseMoveHandler}
+                        onMouseUp={mouseLeaveHandler}
+                    />)}
+                </g>
+                <g>
+                    {Object.keys(props.stateMapData).filter(item => props.stateMapData[item].acres === "0").map(item => <path
+                        className={props.selectedState === item ? 'selected' : ''}
+                        id={item}
+                        key={item}
+                        d={props.stateMapData[item].shape}
+                        fill="#c4c0bc"
+                        stroke="white"
+                        strokeWidth="2px"
+                        acres={props.stateMapData[item].acres}
+                        onMouseEnter={mouseEnterHandler}
+                        onMouseLeave={mouseLeaveHandler}
+                        onMouseMove={mouseMoveHandler}
+                    />)}
+                </g>
+                <g className="line-pattern">
+                    {[...new Array(300).keys()].map(item => <line x1="0" y1={-100 + item * 10} x2="1500" y2={-600 + item * 10} stroke="#ffffff" strokeWidth="2px" clip-path="url(#selectionMask)" />)}
+                </g>
             </svg>
             <div>
                 <Legend />
