203f8f00132118951e512599ea3669ac7e1eae1b
Vicky Lu <vicky.lu@OVX1K92C6DR9MAC.attlocal.net>
2022-06-28 19:26:53 -0400

    map data method update, new build

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import StateMap from './components/StateMap';
-import axios from 'axios';
 import { useState } from 'react';
 import DataTable from './components/DataTable';
 import StateDropdownBtn from './components/StateDropdownBtn';
+
 import stateData from '../src/stateData.js';
 
 function App() {
-
-  // console.log(stateData);
-
   const [selectedState, setSelectedState] = useState("");
-  const [currentStateShape, setStateShape] = useState('');
 
   const onStateSelectHandler = (e) => {
-    console.log(e);
     setSelectedState(e);
   }
-
-  const [data, setData] = useState({});
-  
-  axios.get("https://love2create.github.io/InteractiveStateMap/src/data/stateData.json").then(response=>{
-    setData(response.data);
-  });
 
   return (
     <main>
       <h1>Organic Valley Acres Map</h1>
-      <DataTable stateMapData={data} />
-      <StateDropdownBtn stateMapData={data} onStateSelectHandler={onStateSelectHandler} selectedState={selectedState} />
-      <StateMap stateMapData={data} onStateSelectHandler={onStateSelectHandler} selectedState={selectedState}/>
+      <DataTable stateMapData={stateData} />
+      <StateDropdownBtn stateMapData={stateData} onStateSelectHandler={onStateSelectHandler} selectedState={selectedState} />
+      <StateMap stateMapData={stateData} onStateSelectHandler={onStateSelectHandler} selectedState={selectedState}/>
     </main>
   );
 }
---
+++ src/components/ArrowGraphic.js
@@ -0,0 +1,12 @@
+import './ArrowGraphic.css';
+
+const ArrowGraphic = () => {
+
+    return (
+        <svg className="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.96 7.48">
+            <polygon points="7.48 7.48 0 0 14.96 0 7.48 7.48" fill="currentColor" width="10px" height="7px" />
+        </svg>
+    );
+}
+
+export default ArrowGraphic;
